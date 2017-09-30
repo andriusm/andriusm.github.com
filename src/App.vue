4953182b5a8c5628f45d098a173dd05c2624b0c0
Andrius Miasnikovas <andriusms@gmail.com>
2017-09-30 22:58:43 +0300

    Add toolbar and tabs

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <div id="app">
-    <bookmarklet></bookmarklet>
+    <md-toolbar>
+      <h2 class="md-title" style="flex: 1">Experiments 'n stuff</h2>
+    </md-toolbar>
+
+    <md-tabs>
+      <md-tab id="intro" md-label="Intro">
+        <p>These are some of the experiments, tools and hacks that I thre together.</p>
+      </md-tab>
+
+      <md-tab id="bookmarklet" md-label="Bookmarklet">
+        <bookmarklet></bookmarklet>
+      </md-tab>
+    </md-tabs>
   </div>
 </template>
 
@@ -20,6 +32,5 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  margin: 40px;
 }
 </style>
